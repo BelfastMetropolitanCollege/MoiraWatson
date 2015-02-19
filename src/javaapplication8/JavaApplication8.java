511e1854737e3328321d8678dc4ab831c4d83c1b
pdoherty@belfastmet.ac.uk <>
2015-02-19 15:13:28 +0000

    fgdgsdfgsdfgsdfgsdfgsdfgfghfncvbncvbncv

---
+++ src/javaapplication8/JavaApplication8.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication8;
+
+/**
+ *
+ * @author Peter Doherty <pdoherty@belfastmet.ac.uk>
+ */
+public class JavaApplication8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("dsfsfsfsdf");
+    }
+}
